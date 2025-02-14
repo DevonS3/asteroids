c949a2f54de52090dc0531863badf16a3f8c5391
DevonS3 <really_easy_to_remember@msn.com>
2025-02-14 00:18:37 -0800

    Added player movement

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Create pygame Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Add classes to Groups
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+    
         screen.fill("black")
+
+        updatable.update(dt)
+        for i in drawable:
+            i.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
